097b3c15eb44b4e3590823824e94d184ea0960fa
ambroisegithub <muhayimana21@gmail.com>
2023-12-01 16:41:12 +0200

    laravel Product api

--- app/Http/Controllers/productController.php
+++ app/Http/Controllers/productController.php
@@ -4,17 +4,72 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 
-class productController extends Controller
+class ProductController extends Controller
 {
-    //
     public function index()
     {
         return Product::all();
     }
+
     public function store(Request $request)
     {
-        return Product::create($request->all());
+        try {
+            $request->validate([
+                'name' => 'required',
+                'slug' => 'required',
+                'price' => 'required',
+                'discription' => 'required', // corrected the field name
+            ]);
+
+            $product = Product::create($request->all());
+
+            return response()->json(['message' => 'Product created successfully', 'data' => $product], 201);
+        } catch (ValidationException $e) {
+            return response()->json(['errors' => $e->validator->errors()], 422);
+        }
+    }
+
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        if ($product) {
+            return response()->json(['message' => 'Product retrieved successfully', 'data' => $product]);
+        } else {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+    }
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        if ($product) {
+            $product->update($request->all());
+            return response()->json(['message' => 'Product Updated Successfully', 'data' => $product]);
+        } else {
+            return response()->json(['message' => 'Product Not Found'], 404);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        if ($product) {
+            Product::destroy($id);
+            return response()->json(['message' => 'Product Deleted Succussfully', 'data' => $product]);
+        } else {
+            return response()->json(['message' => 'Product Not found'], 404);
+        }
+    }
+
+    public function deleteAll()
+    {
+        Product::truncate(); // Deletes all records from the products table
+        return response()->json(['message' => 'All products deleted successfully']);
+    }
+
+    public function search($name){
+        return Product::where('name','like','%'.$name.'%')->get();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/product', [productController::class, 'index']);
-
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::patch('/products/{id}', [productController::class, 'update']);
+Route::delete('/products/{id}', [productController::class, 'destroy']);
+Route::delete('/products', [ProductController::class, 'deleteAll']); // New route for deleting all products
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
 // Route::post('/products', function () {
 //     return Product::create([
 //         'name' => 'Product One',
